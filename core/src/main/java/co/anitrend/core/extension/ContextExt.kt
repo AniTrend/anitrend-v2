91d7cf9d51e15f60616f646e7246506c1fa316f5
Maxwell <mxt.developer@gmail.com>
2019-04-23 12:34:42 +0200

    Dependency Injection, Refactoring & Corrections
    Breaking changes including:
    - Injection of Settings, Databases, AuthenticationHelper and Presenters
    
    Corrections Including:
    - MediaQuery incorrect sorting list type which was set to list of annotations instead of strings

--- core/src/main/java/co/anitrend/core/extension/ContextExt.kt
+++ core/src/main/java/co/anitrend/core/extension/ContextExt.kt
@@ -2,18 +2,17 @@ package co.anitrend.core.extension
 
 import android.content.Context
 import co.anitrend.core.api.RetroFactory
-import co.anitrend.core.dao.DatabaseHelper
 import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
 
-fun Context.getDatabaseHelper() =
-    DatabaseHelper.getInstance(this)
-
+/**
+ * Extension function to get any type of given generic type
+ */
 inline fun <reified T> getTypeToken(): Type =
     object : TypeToken<T>() {}.type
 
 /**
- * Creates a retrofit endpoint given the application context
+ * Creates a retrofit endpoint of the given class
  */
 inline fun <reified T> Context.getEndPointOf() : T =
     RetroFactory.getInstance(applicationContext)
