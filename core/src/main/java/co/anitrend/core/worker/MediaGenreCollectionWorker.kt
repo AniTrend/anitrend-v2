185d4c476a395b063334de5df4c6c4665284f3ae
Maxwell <mxt.developer@gmail.com>
2019-05-30 23:44:14 +0200

    Updated Core Module: Included Simple Example

--- core/src/main/java/co/anitrend/core/worker/MediaGenreCollectionWorker.kt
+++ core/src/main/java/co/anitrend/core/worker/MediaGenreCollectionWorker.kt
@@ -1,73 +1,55 @@
 package co.anitrend.core.worker
 
 import android.content.Context
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import co.anitrend.data.api.endpoint.MediaEndPoint
-import co.anitrend.data.dao.DatabaseHelper
-import co.anitrend.data.extension.getEndPointOf
-import co.anitrend.data.model.response.general.media.MediaGenre
 import co.anitrend.core.presenter.CorePresenter
-import co.anitrend.data.util.graphql.GraphUtil
-import io.wax911.support.core.controller.SupportRequestClient
+import co.anitrend.data.api.endpoint.MediaEndPoint
+import co.anitrend.data.extension.getEndPointOf
+import co.anitrend.data.source.media.MediaGenreDataSource
 import io.wax911.support.core.worker.SupportWorker
-import io.wax911.support.core.wrapper.extension.isSuccessful
-import kotlinx.coroutines.runBlocking
+import io.wax911.support.data.factory.contract.IRetrofitFactory
+import io.wax911.support.data.model.contract.SupportStateType
+import io.wax911.support.extension.util.SupportConnectivityHelper
 import org.koin.core.KoinComponent
 import org.koin.core.inject
-import timber.log.Timber
 
 class MediaGenreCollectionWorker(
     context: Context,
     workerParameters: WorkerParameters
-): SupportWorker<CorePresenter>(context, workerParameters), KoinComponent {
+): CoroutineWorker(context, workerParameters), KoinComponent {
 
-    override val presenter by inject<CorePresenter>()
-
-    private val supportRequestClient by inject<SupportRequestClient>()
-
-    private val databaseHelper by inject<DatabaseHelper>()
+    private val connectivityHelper by inject<SupportConnectivityHelper>()
+    private val retroFactory by inject<IRetrofitFactory>()
 
     /**
-     * Override this method to do your actual background processing.  This method is called on a
-     * background thread - you are required to **synchronously** do your work and return the
-     * [androidx.work.ListenableWorker.Result] from this method.  Once you return from this
-     * method, the Worker is considered to have finished what its doing and will be destroyed.  If
-     * you need to do your work asynchronously on a thread of your own choice, see
-     * [androidx.work.ListenableWorker].
+     * A suspending method to do your work.  This function runs on the coroutine context specified
+     * by [coroutineContext].
+     * <p>
+     * A CoroutineWorker is given a maximum of ten minutes to finish its execution and return a
+     * [ListenableWorker.Result].  After this time has expired, the worker will be signalled to
+     * stop.
      *
-     *
-     * A Worker is given a maximum of ten minutes to finish its execution and return a
-     * [androidx.work.ListenableWorker.Result].  After this time has expired, the Worker will
-     * be signalled to stop.
-     *
-     * @return The [androidx.work.ListenableWorker.Result] of the computation; note that
-     * dependent work will not execute if you use
-     * [androidx.work.ListenableWorker.Result.failure] or
-     * [androidx.work.ListenableWorker.Result.failure]
+     * @return The [ListenableWorker.Result] of the result of the background work; note that
+     * dependent work will not execute if you return [ListenableWorker.Result.failure]
      */
-    override fun doWork(): Result {
-        val mediaGenresCall =
-            applicationContext.getEndPointOf<MediaEndPoint>()
-                .getMediaGenres(
-                    GraphUtil.getDefaultQuery()
-                )
-        val wrapper = supportRequestClient.executeUsing(mediaGenresCall)
-        if (wrapper.isSuccessful()) {
-            return wrapper.model?.data?.genreCollection?.let {
-                try {
-                    val mediaGenreList = it.map { genre ->
-                        MediaGenre(genre)
-                    }.toTypedArray()
-                    runBlocking {
-                        databaseHelper.mediaGenreDao().insert(*mediaGenreList)
-                    }
-                    Result.success()
-                } catch (e: Exception) {
-                    Timber.e(e)
-                    Result.failure()
-                }
-            } ?: Result.retry()
+    override suspend fun doWork(): Result {
+        if (connectivityHelper.isConnected) {
+            // TODO: Implement a checking mechanism for the refresh interval for genres to
+            //  avoid refreshing the database every time on request
+            val endpoint = retroFactory.getEndPointOf<MediaEndPoint>()
+            val mediaTagDataSource = MediaGenreDataSource(endpoint)
+
+            val networkResult = mediaTagDataSource.startRequestForType()
+
+            if (networkResult.code == SupportStateType.CONTENT)
+                return Result.success()
+            return Result.failure()
         }
         return Result.retry()
+    }
+
+    companion object {
+        const val TAG = "MediaGenreCollectionWorker"
     }
 }
