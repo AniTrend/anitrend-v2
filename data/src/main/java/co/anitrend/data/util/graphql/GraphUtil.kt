7bc54439a9fc5337253c45bee8e1d5f30f190083
Maxwell <mxt.developer@gmail.com>
2019-06-05 15:01:25 +0200

    Sorting Order Helper & Settings Entry

--- data/src/main/java/co/anitrend/data/util/graphql/GraphUtil.kt
+++ data/src/main/java/co/anitrend/data/util/graphql/GraphUtil.kt
@@ -2,12 +2,19 @@ package co.anitrend.data.util.graphql
 
 import co.anitrend.data.arch.AniTrendExperimentalFeature
 import co.anitrend.data.model.query.PageQuery
+import co.anitrend.data.util.Settings
 import io.github.wax911.library.model.request.QueryContainerBuilder
 
 /**
  * Graph request helper class
  */
 object GraphUtil {
+
+    private val SORT_ORDER_EXCEPTIONS = listOf(
+        "SEARCH_MATCH"
+    )
+
+    private const val SORT_ORDER_DESC_POSTFIX = "_DESC"
 
     /**
      * Default per page loading limit for this application
@@ -39,5 +46,18 @@ object GraphUtil {
                 .replaceFirst("query", "query ")
         }
         return rawData
+    }
+
+    /**
+     * Applies order on sortable keys, if the key is not among the sort order exceptions
+     */
+    fun applySortOrderOn(sortType: String, settings: Settings): String{
+        if (settings.isSortOrderDescending) {
+            return if (SORT_ORDER_EXCEPTIONS.contains(sortType))
+                sortType
+            else
+                sortType + SORT_ORDER_DESC_POSTFIX
+        }
+        return sortType
     }
 }
