0445ef87c37d5f7d30461ec2595d3b0905748a53
Maxwell <mxt.developer@gmail.com>
2019-06-10 23:06:32 +0200

    Refactoring: Code Cleanup

--- core/src/main/java/co/anitrend/core/worker/MediaGenreCollectionWorker.kt
+++ core/src/main/java/co/anitrend/core/worker/MediaGenreCollectionWorker.kt
@@ -1,15 +1,13 @@
 package co.anitrend.core.worker
 
 import android.content.Context
-import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import co.anitrend.core.presenter.CorePresenter
 import co.anitrend.data.api.endpoint.MediaEndPoint
 import co.anitrend.data.extension.getEndPointOf
 import co.anitrend.data.source.media.MediaGenreDataSource
-import io.wax911.support.core.worker.SupportWorker
+import io.wax911.support.core.worker.SupportCoroutineWorker
 import io.wax911.support.data.factory.contract.IRetrofitFactory
-import io.wax911.support.data.model.contract.SupportStateType
 import io.wax911.support.extension.util.SupportConnectivityHelper
 import org.koin.core.KoinComponent
 import org.koin.core.inject
@@ -17,7 +15,7 @@ import org.koin.core.inject
 class MediaGenreCollectionWorker(
     context: Context,
     workerParameters: WorkerParameters
-): CoroutineWorker(context, workerParameters), KoinComponent {
+) : SupportCoroutineWorker<CorePresenter>(context, workerParameters), KoinComponent {
 
     private val connectivityHelper by inject<SupportConnectivityHelper>()
     private val retroFactory by inject<IRetrofitFactory>()
@@ -25,13 +23,13 @@ class MediaGenreCollectionWorker(
     /**
      * A suspending method to do your work.  This function runs on the coroutine context specified
      * by [coroutineContext].
-     * <p>
+     *
      * A CoroutineWorker is given a maximum of ten minutes to finish its execution and return a
-     * [ListenableWorker.Result].  After this time has expired, the worker will be signalled to
+     * [androidx.work.ListenableWorker.Result].  After this time has expired, the worker will be signalled to
      * stop.
      *
-     * @return The [ListenableWorker.Result] of the result of the background work; note that
-     * dependent work will not execute if you return [ListenableWorker.Result.failure]
+     * @return The [androidx.work.ListenableWorker.Result] of the result of the background work; note that
+     * dependent work will not execute if you return [androidx.work.ListenableWorker.Result.failure]
      */
     override suspend fun doWork(): Result {
         if (connectivityHelper.isConnected) {
@@ -42,7 +40,7 @@ class MediaGenreCollectionWorker(
 
             val networkResult = mediaTagDataSource.startRequestForType()
 
-            if (networkResult.code == SupportStateType.CONTENT)
+            if (networkResult.isLoaded())
                 return Result.success()
             return Result.failure()
         }
