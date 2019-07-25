e0d6f7e8b0279f62f85bc7c2bac1caace94d83d7
Maxwell <mxt.developer@gmail.com>
2019-07-25 16:01:45 +0200

    Add Activity Union Contract (#24)

--- data/src/main/java/co/anitrend/data/model/response/core/activity/contract/IFeed.kt
+++ data/src/main/java/co/anitrend/data/model/response/core/activity/contract/IFeed.kt
@@ -18,11 +18,28 @@
 package co.anitrend.data.model.response.core.activity.contract
 
 import co.anitrend.data.entity.contract.IEntity
+import co.anitrend.data.model.response.core.user.contract.IUser
+import co.anitrend.data.usecase.activity.attributes.ActivityType
 
 /** [Activity](https://anilist.github.io/ApiV2-GraphQL-Docs/activityunion.doc.html)
  * Activity contract with shared objects
  *
+ * @property createdAt The time the activity was created at
+ * @property likes The users who liked the activity
+ * @property replyCount The number of activity replies
+ * @property siteUrl The url for the activity page on the AniList website
+ * @property text The status text (Markdown)
+ * @property type The type of activity
+ * @property user The user who created the activity
+ * @property userId The user id of the activity's creator
  */
-interface IActivity : IEntity {
-
+interface IFeed : IEntity {
+    val createdAt: Long
+    val likes: List<IUser>?
+    val replyCount: Int
+    val siteUrl: String?
+    val text: String?
+    val type: ActivityType?
+    val user: IUser?
+    val userId: Long?
 }
