d315a33f8f45477a4c94ad3373fd04ff2438ea70
Maxwell <mxt.developer@gmail.com>
2019-08-11 23:25:37 +0200

    Update Reviews Contract, Connection, Edge and Concrete Models (#24)

--- data/src/main/java/co/anitrend/data/model/response/core/review/contract/IReview.kt
+++ data/src/main/java/co/anitrend/data/model/response/core/review/contract/IReview.kt
@@ -18,11 +18,44 @@
 package co.anitrend.data.model.response.core.review.contract
 
 import co.anitrend.data.entity.contract.IEntity
+import co.anitrend.data.model.response.core.media.contract.IMedia
+import co.anitrend.data.model.response.core.user.contract.IUser
+import co.anitrend.data.usecase.media.attributes.MediaType
+import co.anitrend.data.usecase.review.attributes.ReviewRating
 
 /** [Review](Notification](https://anilist.github.io/ApiV2-GraphQL-Docs/review.doc.html)
- * A Review that features in an anime or manga
+ * Review contract
  *
+ * @property body The main review body text
+ * @property createdAt The time of the thread creation
+ * @property media The media the review is of
+ * @property mediaId The id of the review's media
+ * @property mediaType For which type of media the review is for
+ * @property private If the review is not yet publicly published and is only viewable by creator
+ * @property rating The total user rating of the review
+ * @property ratingAmount The amount of user ratings of the review
+ * @property score The review score of the media
+ * @property siteUrl The url for the review page on the AniList website
+ * @property summary A short summary of the review
+ * @property updatedAt The time of the thread last update
+ * @property user The creator of the review
+ * @property userId The id of the review's creator
+ * @property userRating The rating of the review by currently authenticated user
  */
 interface IReview : IEntity {
-
+    val body: String?
+    val createdAt: Long
+    val media: IMedia?
+    val mediaId: Long
+    val mediaType: MediaType?
+    val private: Boolean?
+    val rating: Int?
+    val ratingAmount: Int?
+    val score: Int?
+    val siteUrl: String?
+    val summary: String?
+    val updatedAt: Long
+    val user: IUser?
+    val userId: Long
+    val userRating: ReviewRating?
 }
