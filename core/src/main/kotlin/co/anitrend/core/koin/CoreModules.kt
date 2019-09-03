8aa41c85c70e0064db6e8f25709d9e45f777728b
Maxwell <mxt.developer@gmail.com>
2019-09-03 11:55:02 +0200

    Add UseCase Modules and Initial View Models

--- core/src/main/kotlin/co/anitrend/core/koin/CoreModules.kt
+++ core/src/main/kotlin/co/anitrend/core/koin/CoreModules.kt
@@ -18,14 +18,19 @@
 package co.anitrend.core.koin
 
 import co.anitrend.core.presenter.CorePresenter
+import co.anitrend.core.viewmodel.MediaGenreViewModel
+import co.anitrend.core.viewmodel.MediaTagViewModel
+import co.anitrend.data.usecase.media.MediaGenreUseCaseImpl
+import co.anitrend.data.usecase.media.MediaTagUseCaseImpl
 import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val coreModules = module {
+private val commonAppModules = module {
 
 }
 
-val corePresenterModules = module {
+private val presenterModules = module {
     factory {
         CorePresenter(
             androidContext(),
@@ -34,6 +39,18 @@ val corePresenterModules = module {
     }
 }
 
-val coreViewModelModules = module {
+private val viewModelModules = module {
+    viewModel {
+        MediaGenreViewModel(
+            useCase = get<MediaGenreUseCaseImpl>()
+        )
+    }
 
+    viewModel {
+        MediaTagViewModel(
+            useCase = get<MediaTagUseCaseImpl>()
+        )
+    }
 }
+
+val coreModules = listOf(commonAppModules, presenterModules, viewModelModules)
--- core/src/main/kotlin/co/anitrend/core/viewmodel/MediaTagViewModel.kt
+++ core/src/main/kotlin/co/anitrend/core/viewmodel/MediaTagViewModel.kt
@@ -15,18 +15,13 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package co.anitrend.core.presenter
+package co.anitrend.core.viewmodel
 
-import android.content.Context
-import androidx.work.BackoffPolicy
-import androidx.work.ExistingWorkPolicy
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
-import co.anitrend.data.util.Settings
-import co.anitrend.arch.core.presenter.SupportPresenter
-import java.util.concurrent.TimeUnit
+import co.anitrend.arch.core.viewmodel.SupportViewModel
+import co.anitrend.arch.data.model.UserInterfaceState
+import co.anitrend.data.model.core.media.MediaTag
+import co.anitrend.domain.usecases.media.MediaTagUseCase
 
-class CorePresenter(
-    context: Context,
-    settings: Settings
-): SupportPresenter<Settings>(context, settings)
+class MediaTagViewModel(
+    override val useCase: MediaTagUseCase<UserInterfaceState<List<MediaTag>>>
+) : SupportViewModel<Nothing?, List<MediaTag>>()
