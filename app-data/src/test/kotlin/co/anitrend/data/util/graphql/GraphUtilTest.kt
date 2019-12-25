65e6dafe58f9f26d7a710afd1c0645da23499115
Maxwell <mxt.developer@gmail.com>
2019-12-25 13:55:17 +0200

    Move graphql tests from instrumented test to plain tests

--- app-data/src/test/kotlin/co/anitrend/data/util/graphql/GraphUtilTest.kt
+++ app-data/src/test/kotlin/co/anitrend/data/util/graphql/GraphUtilTest.kt
@@ -18,6 +18,11 @@
 package co.anitrend.data.util.graphql
 
 import co.anitrend.data.arch.common.model.paging.PageQuery
+import co.anitrend.data.settings.ISortOrderSettings
+import co.anitrend.domain.medialist.enums.MediaListSort
+import co.anitrend.data.util.graphql.GraphUtil.applySortOrderUsing
+import co.anitrend.domain.media.enums.MediaSort
+import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -36,5 +41,66 @@ class GraphUtilTest {
         assertEquals(1, actual.getVariable("page"))
         assertTrue(actual.containsKey("perPage"))
         assertEquals(GraphUtil.PAGING_LIMIT, actual.getVariable("perPage"))
+    }
+
+    @Test
+    fun `sort order utility on enum attaches sort order correctly`() {
+        val expected = "ADDED_TIME_DESC"
+
+        val sortType = MediaListSort.ADDED_TIME
+
+        val settings = object : ISortOrderSettings {
+            override var isSortOrderDescending: Boolean = true
+        }
+
+        val actual = sortType.applySortOrderUsing(settings)
+
+        assertEquals(expected, actual)
+    }
+
+
+    @Test
+    fun `sorting helper does not append postfix to ignored values`() {
+        val given = object : ISortOrderSettings {
+            override var isSortOrderDescending = true
+        }
+
+        val expected = "SEARCH_MATCH"
+
+        val actual = MediaSort.SEARCH_MATCH.applySortOrderUsing(
+            settings = given
+        )
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `sorting helper appends postfix to non ignored values`() {
+        val given = object : ISortOrderSettings {
+            override var isSortOrderDescending = true
+        }
+
+        val expected = "PROGRESS_VOLUMES_DESC"
+
+        val actual = MediaListSort.PROGRESS_VOLUMES.applySortOrderUsing(
+            settings = given
+        )
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `sorting helper does not append postfix when preference order is not set to descending order`() {
+        val given = object : ISortOrderSettings {
+            override var isSortOrderDescending = false
+        }
+
+        val expected = "PROGRESS_VOLUMES"
+
+        val actual = MediaListSort.PROGRESS_VOLUMES.applySortOrderUsing(
+            settings = given
+        )
+
+        assertEquals(expected, actual)
     }
 }
