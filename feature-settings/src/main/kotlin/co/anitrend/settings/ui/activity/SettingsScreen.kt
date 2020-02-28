768d6af8a4956dfa427db861e57356f0fd962f7e
Maxwell <mxt.developer@gmail.com>
2020-02-28 20:24:26 +0200

    Add extra placeholder settings entries

--- feature-settings/src/main/kotlin/co/anitrend/settings/ui/activity/SettingsScreen.kt
+++ feature-settings/src/main/kotlin/co/anitrend/settings/ui/activity/SettingsScreen.kt
@@ -15,48 +15,53 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
- * Copyright (C) 2019  AniTrend
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package co.anitrend.settings.ui.activity
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
+import androidx.fragment.app.commit
+import co.anitrend.core.presenter.CorePresenter
+import co.anitrend.core.ui.activity.AnitrendActivity
 import co.anitrend.settings.R
+import co.anitrend.settings.ui.fragment.SettingsFragment
+import kotlinx.android.synthetic.main.settings_activity.*
+import org.koin.android.ext.android.inject
 
-class SettingsScreen : AppCompatActivity() {
+class SettingsScreen : AnitrendActivity<Nothing, CorePresenter>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(
-                R.id.settings,
-                SettingsFragment()
-            )
-            .commit()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setSupportActionBar(bottomAppBar)
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    /**
+     * Should be created lazily through injection or lazy delegate
+     *
+     * @return supportPresenter of the generic type specified
+     */
+    override val supportPresenter by inject<CorePresenter>()
+
+    /**
+     * Additional initialization to be done in this method, if the overriding class is type of
+     * [androidx.fragment.app.Fragment] then this method will be called in
+     * [androidx.fragment.app.FragmentActivity.onCreate]. Otherwise
+     * [androidx.fragment.app.FragmentActivity.onPostCreate] invokes this function
+     *
+     * @param savedInstanceState
+     */
+    override fun initializeComponents(savedInstanceState: Bundle?) {
+        onUpdateUserInterface()
+    }
+
+    /**
+     * Handles the updating of views, binding, creation or state change, depending on the context
+     * [androidx.lifecycle.LiveData] for a given [ISupportFragmentActivity] will be available by this point.
+     *
+     * Check implementation for more details
+     */
+    override fun onUpdateUserInterface() {
+        supportFragmentManager.commit {
+            replace(R.id.contentFrame, SettingsFragment.newInstance())
         }
     }
 }
