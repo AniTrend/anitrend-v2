cafccd82f58ee2b33c15c1781e28f76bb2d56734
Maxwell <mxt.developer@gmail.com>
2020-03-07 23:22:05 +0200

    Add empty response stub in GraphQLMapper

--- app-data/src/main/kotlin/co/anitrend/data/arch/mapper/GraphQLMapper.kt
+++ app-data/src/main/kotlin/co/anitrend/data/arch/mapper/GraphQLMapper.kt
@@ -19,9 +19,18 @@ package co.anitrend.data.arch.mapper
 
 import co.anitrend.arch.data.mapper.SupportResponseMapper
 import io.github.wax911.library.model.body.GraphContainer
+import timber.log.Timber
 
 /**
  * GraphQLMapper specific mapper, assures that all requests respond with [GraphContainer] as the root tree object.
  * Making it easier for us to implement error logging and provide better error messages
  */
-internal abstract class GraphQLMapper<S, D> : SupportResponseMapper<GraphContainer<S>, D>()
+internal abstract class GraphQLMapper<S, D> : SupportResponseMapper<GraphContainer<S>, D>() {
+
+    /**
+     * Simple logger for empty response
+     */
+    protected fun onEmptyResponse() {
+        Timber.tag(moduleTag).i("onResponseDatabaseInsert -> mappedData is empty")
+    }
+}
