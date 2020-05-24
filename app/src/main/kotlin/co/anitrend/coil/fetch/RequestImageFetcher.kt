211745412f81d5c1de50cf067de140cb15560fb1
Maxwell <mxt.developer@gmail.com>
2020-05-24 15:36:47 +0200

    Add image fetcher component for coil
    Adds support for requesting images using image object and handling cache key generation
    
    See commit: a0f6c1bd955765ca84d5a205e08ababf6af2024a
    
    Closes #56

---
+++ app/src/main/kotlin/co/anitrend/coil/fetch/RequestImageFetcher.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2020  AniTrend
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.anitrend.coil.fetch
+
+import co.anitrend.coil.mapper.RequestImageMapper
+import co.anitrend.core.android.helpers.image.model.RequestImage
+import coil.bitmappool.BitmapPool
+import coil.decode.DataSource
+import coil.decode.Options
+import coil.fetch.FetchResult
+import coil.fetch.Fetcher
+import coil.fetch.SourceResult
+import coil.size.Size
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import timber.log.Timber
+
+class RequestImageFetcher(
+    private val client: OkHttpClient,
+    private val mapper: RequestImageMapper
+) : Fetcher<RequestImage<*>> {
+
+    private fun fetchDataSourceUsing(httpUrl: HttpUrl) = runCatching {
+        val request = Request.Builder()
+            .get().url(httpUrl).build()
+        val call = client.newCall(request)
+
+        @Suppress("BlockingMethodInNonBlockingContext")
+        val response = call.execute()
+        response.body?.source()
+    }.getOrElse {
+        Timber.tag(moduleTag).i(it)
+        null
+    }
+
+
+    /**
+     * Load the [data] into memory. Perform any necessary fetching operations.
+     *
+     * @param pool A [BitmapPool] which can be used to request [Bitmap] instances.
+     * @param data The data to load.
+     * @param size The requested dimensions for the image.
+     * @param options A set of configuration options for this request.
+     */
+    override suspend fun fetch(
+        pool: BitmapPool,
+        data: RequestImage<*>,
+        size: Size,
+        options: Options
+    ): FetchResult {
+        val url = mapper.map(data, size)
+        // TODO: will probably crash on a network error, unless coil handles exceptions internally
+        val source = fetchDataSourceUsing(url)!!
+        return SourceResult(
+            source = source,
+            mimeType = "image/*",
+            dataSource = DataSource.NETWORK
+        )
+    }
+
+    /**
+     * Compute the memory cache key for [data].
+     *
+     * Items with the same cache key will be treated as equivalent by the [MemoryCache].
+     *
+     * Returning null will prevent the result of [fetch] from being added to the memory cache.
+     */
+    override fun key(data: RequestImage<*>): String? {
+        return mapper.getImageUrlUsing(data)
+    }
+
+    companion object {
+        private val moduleTag = RequestImageFetcher::class.java.simpleName
+    }
+}
