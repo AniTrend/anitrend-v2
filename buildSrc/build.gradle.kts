818a42b2a7803a7bccd8d644c848adf7d85f0b63
Maxwell <mxt.developer@gmail.com>
2020-06-17 22:45:01 +0200

    Minor comment corrections (#59)
    See commit: 6a1d59d6d3cc437bd8789edef454990bcb75bce7

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -29,10 +29,10 @@ val kotlinVersion = "1.3.72"
 val buildToolsVersion = "4.0.0"
 
 dependencies {
-    /* Depend on the android gradle plugin, since we want to access it in our plugin */
+    /** Depend on the android gradle plugin, since we want to access it in our plugin */
     implementation("com.android.tools.build:gradle:$buildToolsVersion")
 
-    /* Depend on the kotlin plugin, since we want to access it in our plugin */
+    /** Depend on the kotlin plugin, since we want to access it in our plugin */
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
     /* Depend on the default Gradle API's since we want to build a custom plugin */
