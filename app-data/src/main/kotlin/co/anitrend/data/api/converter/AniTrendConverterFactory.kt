447d4b396398829e1336f3a48d087ae6d1a60892
Maxwell <mxt.developer@gmail.com>
2020-07-02 12:32:58 +0200

    Rewrite converter factory in response to retrofit-graphql bump
    See commit: 3e04930cfddaf1c02bd8b081258446f8a451f46f

--- app-data/src/main/kotlin/co/anitrend/data/api/converter/AniTrendConverterFactory.kt
+++ app-data/src/main/kotlin/co/anitrend/data/api/converter/AniTrendConverterFactory.kt
@@ -17,16 +17,11 @@
 
 package co.anitrend.data.api.converter
 
-import android.content.Context
 import co.anitrend.data.api.converter.request.AniRequestConverter
 import co.anitrend.data.arch.JSON
-import co.anitrend.data.arch.XML
-import com.google.gson.ExclusionStrategy
-import com.google.gson.FieldAttributes
-import com.google.gson.GsonBuilder
+import com.google.gson.Gson
+import io.github.wax911.library.annotation.processor.contract.AbstractGraphProcessor
 import io.github.wax911.library.converter.GraphConverter
-import io.github.wax911.library.model.request.QueryContainerBuilder
-import io.github.wax911.library.util.LogLevel
 import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Converter
@@ -35,8 +30,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.reflect.Type
 
 internal class AniTrendConverterFactory(
-    context: Context
-) : GraphConverter(context) {
+    processor: AbstractGraphProcessor,
+    gson: Gson
+) : GraphConverter(processor, gson) {
 
     /**
      * Response body converter delegates logic processing to a child class that handles
@@ -50,14 +46,14 @@ internal class AniTrendConverterFactory(
      * @see AniRequestConverter
      */
     override fun requestBodyConverter(
-        type: Type?,
-        parameterAnnotations: Array<Annotation>,
-        methodAnnotations: Array<Annotation>,
-        retrofit: Retrofit?
-    ): Converter<QueryContainerBuilder, RequestBody>? =
+        type: Type,
+        parameterAnnotations: Array<out Annotation>,
+        methodAnnotations: Array<out Annotation>,
+        retrofit: Retrofit
+    ): Converter<*, RequestBody>? =
         AniRequestConverter(
             methodAnnotations = methodAnnotations,
-            graphProcessor = graphProcessor,
+            processor = graphProcessor,
             gson = gson
         )
 
@@ -73,8 +69,8 @@ internal class AniTrendConverterFactory(
      * @see io.github.wax911.library.converter.response.GraphResponseConverter
      */
     override fun responseBodyConverter(
-        type: Type?,
-        annotations: Array<Annotation>,
+        type: Type,
+        annotations: Array<out Annotation>,
         retrofit: Retrofit
     ): Converter<ResponseBody, *>? {
         for (annotation in annotations) {
@@ -86,53 +82,12 @@ internal class AniTrendConverterFactory(
                     ).responseBodyConverter(type!!, annotations, retrofit)
                 }*/
                 is JSON -> {
-                    return GsonConverterFactory.create(GSON)
-                        .responseBodyConverter(type!!, annotations, retrofit)
+                    return GsonConverterFactory.create(gson)
+                        .responseBodyConverter(type, annotations, retrofit)
                 }
             }
         }
         // Default for GraphQl marked queries
         return super.responseBodyConverter(type, annotations, retrofit)
-    }
-
-    companion object {
-        private val GSON = GsonBuilder()
-            .generateNonExecutableJson()
-            .setLenient()
-            .create()
-
-        private val exclusionStrategy = object : ExclusionStrategy {
-            /**
-             * @param clazz the class object that is under test
-             * @return true if the class should be ignored; otherwise false
-             */
-            override fun shouldSkipClass(clazz: Class<*>?) = false
-
-            /**
-             * @param f the field object that is under test
-             * @return true if the field should be ignored; otherwise false
-             */
-            override fun shouldSkipField(f: FieldAttributes?): Boolean {
-                return f?.name?.equals("operationName") ?: false
-                        ||
-                        f?.name?.equals("extensions") ?: false
-            }
-        }
-
-        /**
-         * Allows you to provide your own Gson configuration which will be used when serialize or
-         * deserialize response and request bodies.
-         *
-         * @param context any valid application context
-         */
-        fun create(context: Context) =
-            AniTrendConverterFactory(context).apply {
-                gson = GsonBuilder()
-                    .addSerializationExclusionStrategy(exclusionStrategy)
-                    .generateNonExecutableJson()
-                    .setLenient()
-                    .create()
-                setLogLevel(LogLevel.ERROR)
-            }
     }
 }
