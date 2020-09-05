640132139b533ff06b482d19f864571f97c3915d
Maxwell <mxt.developer@gmail.com>
2020-09-05 15:46:44 +0200

    Bump androix.core from 1.5.0-alpha01 to 1.5.0-alpha02

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -61,7 +61,7 @@ object Libraries {
         }
 
         object Core {
-            private const val version = "1.5.0-alpha01"
+            private const val version = "1.5.0-alpha02"
             const val core = "androidx.core:core:$version"
             const val coreKtx = "androidx.core:core-ktx:$version"
 
