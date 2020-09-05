645ad57f6b62c22adc5f00c75b2a284fed04a9ab
Maxwell <mxt.developer@gmail.com>
2020-09-05 16:00:28 +0200

    Bump io.coil-kt from 0.11.0 to 1.0.0-rc2

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -304,7 +304,7 @@ object Libraries {
     }
 
     object Coil {
-        private const val version = "0.11.0"
+        private const val version = "1.0.0-rc2"
         const val coil = "io.coil-kt:coil:$version"
         const val base = "io.coil-kt:coil-base:$version"
         const val gif = "io.coil-kt:coil-gif:$version"
