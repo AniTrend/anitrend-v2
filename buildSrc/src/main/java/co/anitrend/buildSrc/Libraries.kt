eee2da3ffa5cab229bc8d6ca2583ab43ba687401
Maxwell <mxt.developer@gmail.com>
2020-09-17 10:04:44 +0200

    Bump kotlin version from 1.4.0 to 1.4.10

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -374,7 +374,7 @@ object Libraries {
         }
 
         object Kotlin {
-            private const val version = "1.4.0"
+            private const val version = "1.4.10"
             const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
             const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$version"
 
