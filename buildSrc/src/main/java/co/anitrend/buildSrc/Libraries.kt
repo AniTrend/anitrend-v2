c08ddd50e4bb3ceea88b8ede652082180972a010
Maxwell <mxt.developer@gmail.com>
2020-09-17 10:07:19 +0200

    Bump anitrend.support-arch to latest bleeding edge

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -236,7 +236,7 @@ object Libraries {
     object AniTrend {
 
         object Arch {
-            private const val version = "0770fc6ace"
+            private const val version = "8bbfed60fe"
             const val ui = "com.github.anitrend.support-arch:support-ui:${version}"
             const val ext = "com.github.anitrend.support-arch:support-ext:${version}"
             const val core = "com.github.anitrend.support-arch:support-core:${version}"
