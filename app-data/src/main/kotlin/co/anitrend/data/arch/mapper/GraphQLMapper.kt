82ad62a23b7d83a439546165c9e105494d98341a
Maxwell <mxt.developer@gmail.com>
2020-09-29 14:06:48 +0200

    Add helper functions for graphql response mapper

--- app-data/src/main/kotlin/co/anitrend/data/arch/mapper/GraphQLMapper.kt
+++ app-data/src/main/kotlin/co/anitrend/data/arch/mapper/GraphQLMapper.kt
@@ -18,6 +18,7 @@
 package co.anitrend.data.arch.mapper
 
 import co.anitrend.arch.data.mapper.SupportResponseMapper
+import co.anitrend.data.arch.railway.OutCome
 import io.github.wax911.library.model.body.GraphContainer
 import timber.log.Timber
 
@@ -28,11 +29,38 @@ import timber.log.Timber
 internal abstract class GraphQLMapper<S, D> : SupportResponseMapper<S, D>() {
 
     /**
-     * Simple logger for empty response
+     * Handles the persistence of [data] into a local source
+     *
+     * @return [OutCome.Pass] or [OutCome.Fail] of the operation
      */
-    protected fun onEmptyResponse() {
-        Timber.tag(moduleTag).v(
-            "onResponseDatabaseInsert -> mappedData is empty"
-        )
+    protected open suspend fun persistChanges(data: D): OutCome<Nothing?> {
+        // optional
+        return OutCome.Pass(null)
+    }
+
+    /**
+     * If [data] is a type of [Collection], checks if empty otherwise checks nullability
+     */
+    protected fun <T> checkValidity(data: T?): OutCome<T> {
+        if (data == null)
+            return OutCome.Fail(
+                listOf(Throwable("Data validity failed data is null"))
+            )
+
+        if (data is Collection<*> && data.isEmpty())
+            return OutCome.Fail(
+                listOf(Throwable("Data validity failed collection is empty"))
+            )
+
+        return OutCome.Pass(data)
+    }
+
+    /**
+     * Logs [exceptions] of failed operations
+     */
+    protected fun handleException(exceptions: List<Throwable>) {
+        exceptions.forEach {
+            Timber.tag(moduleTag).w(it, "Unhandled exception thrown")
+        }
     }
 }
