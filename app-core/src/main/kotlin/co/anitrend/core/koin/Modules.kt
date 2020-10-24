4bbe5a619d907698d5a229f8c80fb6cf0c0e1978
Maxwell <mxt.developer@gmail.com>
2020-10-24 12:08:35 +0200

    Refactor type definitions exposed by koin for configuration utilities
    See commit: 1affb6ffac550d84fa4ddca2f44f37432d4c403e

--- app-core/src/main/kotlin/co/anitrend/core/koin/Modules.kt
+++ app-core/src/main/kotlin/co/anitrend/core/koin/Modules.kt
@@ -36,8 +36,11 @@ import co.anitrend.core.helper.StorageHelper
 import co.anitrend.core.settings.Settings
 import co.anitrend.core.settings.common.cache.ICacheSettings
 import co.anitrend.core.util.config.ConfigurationUtil
+import co.anitrend.core.util.config.contract.IConfigurationUtil
 import co.anitrend.core.util.locale.LocaleHelper
+import co.anitrend.core.util.locale.contract.ILocaleHelper
 import co.anitrend.core.util.theme.ThemeHelper
+import co.anitrend.core.util.theme.contract.IThemeHelper
 import co.anitrend.data.arch.di.dataModules
 import coil.ImageLoader
 import coil.decode.GifDecoder
@@ -71,7 +74,7 @@ private val coreModule = module {
         )
     } bind IStateLayoutConfig::class
 
-    factory {
+    factory<IConfigurationUtil> {
         ConfigurationUtil(
             settings = get(),
             localeHelper = get(),
@@ -98,19 +101,19 @@ private val coreModule = module {
     } bind PowerController::class
 
     factory {
-        val localeHelper = get<LocaleHelper>()
+        val localeHelper = get<ILocaleHelper>()
         PrettyTime(localeHelper.locale)
     }
 }
 
 private val configurationModule = module {
-    single {
+    single<ILocaleHelper> {
         LocaleHelper(
             settings = get()
         )
     }
 
-    single {
+    single<IThemeHelper> {
         ThemeHelper(
             settings = get()
         )
