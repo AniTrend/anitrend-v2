fd20c1435bf2909db98baa12dec90eb6f4f90eac
Maxwell <mxt.developer@gmail.com>
2020-10-24 11:34:03 +0200

    Bump junit from 4.13 to 4.13.1

--- buildSrc/src/main/java/co/anitrend/buildSrc/common/Versions.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/common/Versions.kt
@@ -44,7 +44,7 @@ object Versions {
     const val versionName = "$major.$minor.$patch-alpha$candidate"
 
     const val mockk = "1.10.2"
-    const val junit = "4.13"
+    const val junit = "4.13.1"
 
     const val timber = "4.7.1"
     const val threeTenBp = "1.2.4"
