d39b8a05030b4aa7260e7ecd074c157ed5838424
Maxwell <mxt.developer@gmail.com>
2020-10-24 11:13:58 +0200

    Bump com.google.android.exoplayer from 2.11.7 to r2.12.1
    Changelog: https://github.com/google/ExoPlayer/blob/release-v2/RELEASENOTES.md

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -390,7 +390,7 @@ object Libraries {
     object Google {
 
         object Exo {
-            private const val version = "2.11.7"
+            private const val version = "r2.12.1"
             const val workManager = "com.google.android.exoplayer:extension-workmanager:$version"
             const val okHttp = "com.google.android.exoplayer:extension-okhttp:$version"
             const val core = "com.google.android.exoplayer:extension-core:$version"
