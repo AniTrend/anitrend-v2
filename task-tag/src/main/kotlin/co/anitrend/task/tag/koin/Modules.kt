b41d3ed512391a2ae1f6cffdf3d13f935492282d
Maxwell <mxt.developer@gmail.com>
2020-12-08 19:13:25 +0200

    Add genre and tag tasks
    Handles fetching of genres and tags respectively through a worker, which is exposed through `app-navigation`

---
+++ task-tag/src/main/kotlin/co/anitrend/task/tag/koin/Modules.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2020  AniTrend
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.anitrend.task.tag.koin
+
+import co.anitrend.core.koin.helper.DynamicFeatureModuleHelper
+import co.anitrend.navigation.TagTaskRouter
+import co.anitrend.task.tag.component.TagWorker
+import co.anitrend.task.tag.provider.FeatureProvider
+import org.koin.androidx.workmanager.dsl.worker
+import org.koin.dsl.module
+
+private val workManagerModule = module {
+    worker {
+        TagWorker(
+            context = get(),
+            parameters = get(),
+            interactor = get()
+        )
+    }
+}
+
+private val featureModule = module {
+    factory<TagTaskRouter.Provider> {
+        FeatureProvider()
+    }
+}
+
+internal val moduleHelper = DynamicFeatureModuleHelper(
+    listOf(workManagerModule, featureModule)
+)
