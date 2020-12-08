20ab1eb554bcf333f7e1698b635931d9135d34d5
Maxwell <mxt.developer@gmail.com>
2020-12-08 18:11:04 +0200

    Bump kotlinx-coroutines from 1.4.0 to 1.4.2

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -481,7 +481,7 @@ object Libraries {
 
         object KotlinX {
             object Coroutines {
-                private const val version = "1.4.0"
+                private const val version = "1.4.2"
                 const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
                 const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
                 const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
