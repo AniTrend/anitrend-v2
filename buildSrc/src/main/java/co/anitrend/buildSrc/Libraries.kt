91c17e65fc872331943885f0b58b842f1c8b3107
Maxwell <mxt.developer@gmail.com>
2020-12-08 17:59:11 +0200

    Bump androidx.fragment from 1.3.0-beta01 to 1.3.0-beta02

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -132,7 +132,7 @@ object Libraries {
         }
 
         object Fragment {
-            private const val version = "1.3.0-beta01"
+            private const val version = "1.3.0-beta02"
             const val fragment = "androidx.fragment:fragment:$version"
             const val fragmentKtx = "androidx.fragment:fragment-ktx:$version"
             const val test = "androidx.fragment:fragment-ktx:fragment-testing$version"
