a145535ede0693e60187a036ddd33a907dfb60e1
Maxwell <mxt.developer@gmail.com>
2020-12-08 19:34:01 +0200

    Remove explicit coroutine builder implementation with delegates
    Latest version of `support-arch` added delegation for adding implementation details for `ISupportCoroutine` all backed by a `SupervisiorJob`

--- app-core/src/main/kotlin/co/anitrend/core/initializer/contract/AbstractFeatureInitializer.kt
+++ app-core/src/main/kotlin/co/anitrend/core/initializer/contract/AbstractFeatureInitializer.kt
@@ -15,40 +15,17 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
- * Copyright (C) 2020  AniTrend
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package co.anitrend.core.initializer.contract
 
 import androidx.startup.Initializer
-import co.anitrend.arch.extension.coroutine.SupportCoroutine
+import co.anitrend.arch.extension.coroutine.ISupportCoroutine
+import co.anitrend.arch.extension.coroutine.extension.Main
 import co.anitrend.core.initializer.InjectorInitializer
-import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
 
 /**
  * Contract for feature initializer with coroutine support for deferred initialization
  */
-abstract class AbstractFeatureInitializer<T> : Initializer<T>, SupportCoroutine {
-
-    /**
-     * Requires an instance of [kotlinx.coroutines.Job] or [kotlinx.coroutines.SupervisorJob]
-     */
-    final override val supervisorJob: Job = SupervisorJob()
+abstract class AbstractFeatureInitializer<T> : Initializer<T>, ISupportCoroutine by Main() {
 
     /**
      * @return A list of dependencies that this [Initializer] depends on. This is
@@ -59,27 +36,4 @@ abstract class AbstractFeatureInitializer<T> : Initializer<T>, SupportCoroutine 
     override fun dependencies(): List<Class<out Initializer<*>>> {
         return listOf(InjectorInitializer::class.java)
     }
-
-    /**
-     * Coroutine dispatcher specification
-     *
-     * @return one of the sub-types of [kotlinx.coroutines.Dispatchers]
-     */
-    final override val coroutineDispatcher: CoroutineDispatcher = Dispatchers.Default
-
-    /**
-     * Persistent context for the coroutine
-     *
-     * @return [kotlin.coroutines.CoroutineContext] preferably built from
-     * [supervisorJob] + [coroutineDispatcher]
-     */
-    final override val coroutineContext: CoroutineContext = supervisorJob + coroutineDispatcher
-
-    /**
-     * A failure or cancellation of a child does not cause the supervisor job
-     * to fail and does not affect its other children.
-     *
-     * @return [kotlinx.coroutines.CoroutineScope]
-     */
-    final override val scope: CoroutineScope = CoroutineScope(coroutineContext)
 }
