37d6100d2ef4d33a9c32f9ec8bbe7e816de72f99
Maxwell <mxt.developer@gmail.com>
2020-12-08 18:10:24 +0200

    Bump org.jetbrains.kotlin from 1.4.10 to 1.4.21

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -462,7 +462,7 @@ object Libraries {
         }
 
         object Kotlin {
-            private const val version = "1.4.10"
+            private const val version = "1.4.21"
             const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
             const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$version"
 
