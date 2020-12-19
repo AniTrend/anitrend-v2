f12ced534f0ff994c3687049e2aa89898d1587d4
Maxwell <mxt.developer@gmail.com>
2020-12-19 14:23:02 +0200

    Bump markwon from 4.5.1 to 4.6.0

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -514,7 +514,7 @@ object Libraries {
     }
 
     object Markwon {
-        private const val version = "4.5.1"
+        private const val version = "4.6.0"
         const val core = "io.noties.markwon:core:$version"
         const val html = "io.noties.markwon:html:$version"
         const val image = "io.noties.markwon:image:$version"
