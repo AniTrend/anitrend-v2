cd32b00a4b33e9139cb711613883a86d91b094b4
Maxwell <mxt.developer@gmail.com>
2021-01-20 11:45:55 +0200

    Bump com.google.firebase:firebase-analytics [18.0.0 -> 18.0.1]

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -417,7 +417,7 @@ object Libraries {
             const val firebaseCore = "com.google.firebase:firebase-core:$version"
 
             object Analytics {
-                private const val version = "18.0.0"
+                private const val version = "18.0.1"
                 const val analytics = "com.google.firebase:firebase-analytics:$version"
                 const val analyticsKtx = "com.google.firebase:firebase-analytics-ktx:$version"
             }
