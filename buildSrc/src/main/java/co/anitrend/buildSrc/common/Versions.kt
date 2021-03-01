d492a9ae552ff2495d18f0865d47a20973c27ca2
Maxwell <mxt.developer@gmail.com>
2021-03-01 14:05:14 +0200

    Bump junit:junit [4.13.1 -> 4.13.2]
    Changelog: http://junit.org

--- buildSrc/src/main/java/co/anitrend/buildSrc/common/Versions.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/common/Versions.kt
@@ -46,7 +46,7 @@ object Versions {
     const val versionName = "$major.$minor.$patch-$channel$candidate"
 
     const val mockk = "1.10.6"
-    const val junit = "4.13.1"
+    const val junit = "4.13.2"
 
     const val timber = "4.7.1"
     const val threeTenBp = "1.3.0"
