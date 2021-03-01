1c052e5ca6925909b961913977893c6e2c36c250
Maxwell <mxt.developer@gmail.com>
2021-03-01 13:56:09 +0200

    Bump com.github.bumptech.glide:compiler [4.11.0 -> 4.12.0]
    Changelog: https://github.com/bumptech/glide

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -465,7 +465,7 @@ object Libraries {
     }
 
     object Glide {
-        private const val version = "4.11.0"
+        private const val version = "4.12.0"
         const val glide = "com.github.bumptech.glide:glide:$version"
         const val compiler = "com.github.bumptech.glide:compiler:$version"
     }
