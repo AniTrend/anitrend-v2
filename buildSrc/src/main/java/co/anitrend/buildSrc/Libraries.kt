28f08dcbfe3507c716b6a67b6c86ce5c01a461b4
Maxwell <mxt.developer@gmail.com>
2021-03-01 14:08:14 +0200

    Bump org.jetbrains.kotlinx:kotlinx-serialization-json [1.0.1 -> 1.1.0]
    Changelog: https://github.com/Kotlin/kotlinx.serialization

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -503,7 +503,7 @@ object Libraries {
             }
 
             object Serialization {
-                private const val version = "1.0.1"
+                private const val version = "1.1.0"
                 const val json = "org.jetbrains.kotlinx:kotlinx-serialization-json:$version"
             }
         }
