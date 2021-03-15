9a073cc61839ac4c21971245a16f616bd57aa448
Maxwell <mxt.developer@gmail.com>
2021-03-15 11:26:00 +0200

    Update scripts

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 function create_directories {
-    mkdir -p ./app-data/.config/
-    cd ./app-data/.config/
+    mkdir -p ./app-core/src/main/resources/org/koin/core/
+    cd ./app-core/src/main/resources/org/koin/core/
 }
 
 function create_files {
-    touch secrets.properties
-    echo "clientId=\"client_id\"" >> secrets.properties
-    echo "clientSecret=\"client_secret\"" >> secrets.properties
+    touch koin.properties
+    echo "aniListClientId=\"aniListClientId\"" >> koin.properties
+    echo "aniListClientSecret=\"aniListClientSecret\"" >> koin.properties
+    echo "traktClientId=\"traktClientId\"" >> koin.properties
+    echo "traktClientSecret=\"traktClientSecret\"" >> koin.properties
+    echo "tmdbClientSecret=\"tmdbClientSecret\"" >> koin.properties
 }
-
 
 create_directories
 create_files
-
