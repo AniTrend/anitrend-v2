AniTrend/anitrend-v2

commit 3b2025a7af231f4d361955bcf500a56d6551feeb

Remove unique constraint on cache entity
Fixes request caching bug which expected the `cache_item_id` to be unique thus resulting in overwrites

app-data/src/main/kotlin/co/anitrend/data/cache/entity/CacheEntity.kt
@@ -28,8 +28,7 @@ import org.threeten.bp.Instant
     tableName = "cache_log",
     indices = [
         Index(
-            value = ["cache_item_id"],
-            unique = true
+            value = ["cache_item_id"]
         )
     ]
 )

app-data/src/main/kotlin/co/anitrend/data/cache/repository/CacheStorePolicy.kt
@@ -23,6 +23,8 @@ import co.anitrend.data.cache.helper.inPast
 import co.anitrend.data.cache.model.CacheIdentity
 import co.anitrend.data.cache.model.CacheRequest
 import co.anitrend.data.cache.repository.contract.ICacheStorePolicy
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.threeten.bp.Instant
 import org.threeten.bp.temporal.TemporalAmount
 
@@ -33,7 +35,9 @@ internal abstract class CacheStorePolicy : ICacheStorePolicy {
 
     private suspend fun getRequestInstant(
         entityId: Long
-    ) = localSource.getCacheLog(request, entityId)?.timestamp
+    ) = withContext(Dispatchers.IO) {
+        localSource.getCacheLog(request, entityId)?.timestamp
+    }
 
     /**
      * Checks if the given [identity] has been requested before a certain time [instant]
@@ -58,19 +62,23 @@ internal abstract class CacheStorePolicy : ICacheStorePolicy {
      */
     override suspend fun hasBeenRequested(
         identity: CacheIdentity
-    ) = localSource.countMatching(request, identity.id) > 0
+    ) = withContext(Dispatchers.IO) {
+        localSource.countMatching(request, identity.id) > 0
+    }
 
     /**
      * Updates the last request [timestamp] for the given [identity]
      */
     override suspend fun updateLastRequest(identity: CacheIdentity, timestamp: Instant) {
-        localSource.upsert(
-            CacheEntity(
-                request = request,
-                cacheItemId = identity.id,
-                timestamp = timestamp
+        withContext(Dispatchers.IO) {
+            localSource.upsert(
+                CacheEntity(
+                    request = request,
+                    cacheItemId = identity.id,
+                    timestamp = timestamp
+                )
             )
-        )
+        }
     }
 
     /**

app-data/src/main/kotlin/co/anitrend/data/cache/model/CacheRequest.kt
@@ -31,5 +31,6 @@ enum class CacheRequest(val alias: String) {
     NEWS("news"),
     EPISODE("episode"),
     MOE("moe"),
+    JIKAN("jikan"),
     XEM("the_xem"),
 }

commit 9e64ac77c14c84d6e7817df710a991387ce61a93

Fix cache policy keeping multiple entries of same entity type
Adds partial update if cache exists, and by doing so fixes bug introduced by commit: 3b2025a7af231f4d361955bcf500a56d6551feeb

app-data/src/main/kotlin/co/anitrend/data/cache/datasource/CacheLocalSource.kt
@@ -15,33 +15,20 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
- * Copyright (C) 2020  AniTrend
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package co.anitrend.data.cache.datasource
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
 import co.anitrend.data.arch.database.dao.ILocalSource
 import co.anitrend.data.cache.entity.CacheEntity
 import co.anitrend.data.cache.model.CacheRequest
+import org.threeten.bp.Instant
 
 @Dao
 internal abstract class CacheLocalSource : ILocalSource<CacheEntity> {
+
     @Query("""
         delete from cache_log
     """)
@@ -59,11 +46,33 @@ internal abstract class CacheLocalSource : ILocalSource<CacheEntity> {
         """)
     abstract suspend fun countMatching(request: CacheRequest, itemId: Long): Int
 
-
     @Query("""
         select * 
         from cache_log 
         where request = :request and cache_item_id = :itemId
         """)
     abstract suspend fun getCacheLog(request: CacheRequest, itemId: Long): CacheEntity?
+
+    @Query("""
+        select id
+        from cache_log 
+        where request = :request and cache_item_id = :itemId
+        """)
+    abstract suspend fun getCacheLogId(request: CacheRequest, itemId: Long): Long?
+
+
+    @Query("""
+        update cache_log
+        set timestamp = :timeStamp
+        where id = :id
+        """)
+    abstract suspend fun partialUpdate(id: Long, timeStamp: Instant)
+
+    @Transaction
+    open suspend fun insertOrUpdate(entity: CacheEntity) {
+        val id = getCacheLogId(entity.request, entity.cacheItemId)
+        if (id == null)
+            insert(entity)
+        else partialUpdate(id, entity.timestamp)
+    }
 }

app-data/src/main/kotlin/co/anitrend/data/cache/entity/CacheEntity.kt
@@ -49,4 +49,10 @@ internal data class CacheEntity(
             timestamp = Instant.now()
         )
     }
+
+    /**
+     * Returns a string representation of the object.
+     */
+    override fun toString() =
+        "CacheEntity(id: $id, request: $request, cacheItemId: $cacheItemId, timestamp: $timestamp)"
 }

app-data/src/main/kotlin/co/anitrend/data/cache/extensions/CachePolicyExtensions.kt
@@ -40,7 +40,7 @@ internal operator fun ICacheStorePolicy.invoke(
         requestHelper.runIfNotRunning(
             Request.Default(cacheIdentity.key, requestType)
         ) { requestCallback ->
-            if (shouldRefresh(cacheIdentity, cacheIdentity.expiresAfter)) {
+            if (shouldRefresh(cacheIdentity, cacheIdentity.expiresAt)) {
                 if (block(requestCallback))
                     updateLastRequest(cacheIdentity, Instant.now())
             }

app-data/src/main/kotlin/co/anitrend/data/cache/model/CacheIdentity.kt
@@ -25,11 +25,11 @@ import org.threeten.bp.Instant
  *
  * @property id Unique identifier for the resource, typically could be a model id a network request
  * @property key Unique key for the identifier, this can be considered as a grouping key for the [id]
- * @property expiresAfter Time duration which the cached item should be valid for. Default is 3 hours
+ * @property expiresAt Time duration which the cached item should be valid for. Default is 3 hours in the past from now
  */
 internal interface CacheIdentity {
     val id: Long
     val key: String
-    val expiresAfter: Instant
+    val expiresAt: Instant
         get() = instantInPast(hours = 3)
 }

app-data/src/main/kotlin/co/anitrend/data/cache/repository/CacheStorePolicy.kt
@@ -27,6 +27,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.threeten.bp.Instant
 import org.threeten.bp.temporal.TemporalAmount
+import timber.log.Timber
 
 internal abstract class CacheStorePolicy : ICacheStorePolicy {
 
@@ -35,8 +36,10 @@ internal abstract class CacheStorePolicy : ICacheStorePolicy {
 
     private suspend fun getRequestInstant(
         entityId: Long
-    ) = withContext(Dispatchers.IO) {
-        localSource.getCacheLog(request, entityId)?.timestamp
+    ): Instant? = withContext(Dispatchers.IO) {
+        val instant = localSource.getCacheLog(request, entityId)
+        Timber.v("getRequestInstant(entityId: $entityId) -> $instant")
+        instant?.timestamp
     }
 
     /**
@@ -45,7 +48,12 @@ internal abstract class CacheStorePolicy : ICacheStorePolicy {
     override suspend fun isRequestBefore(
         identity: CacheIdentity,
         instant: Instant
-    ) = getRequestInstant(identity.id)?.isBefore(instant) ?: true
+    ): Boolean {
+        val lastRequestInstant = getRequestInstant(identity.id)
+        val isBefore = lastRequestInstant?.isBefore(instant)
+        Timber.v("isRequestBefore(identity: $identity, instant: $instant) -> $isBefore")
+        return isBefore ?: true
+    }
 
     /**
      * Checks if the given [identity] has expired using a [threshold]
@@ -55,15 +63,21 @@ internal abstract class CacheStorePolicy : ICacheStorePolicy {
     override suspend fun isRequestExpired(
         identity: CacheIdentity,
         threshold: TemporalAmount
-    ): Boolean = isRequestBefore(identity, threshold.inPast())
+    ): Boolean {
+        Timber.v("isRequestExpired(identity: $identity, threshold: $threshold)")
+        return isRequestBefore(identity, threshold.inPast())
+    }
 
     /**
      * Checks if the given [identity] has been requested before, regardless of when
      */
     override suspend fun hasBeenRequested(
         identity: CacheIdentity
-    ) = withContext(Dispatchers.IO) {
-        localSource.countMatching(request, identity.id) > 0
+    ): Boolean = withContext(Dispatchers.IO) {
+        val requestCount = localSource.countMatching(request, identity.id)
+        val exists = requestCount > 0
+        Timber.v("hasBeenRequested(identity: $identity) -> $exists")
+        exists
     }
 
     /**
@@ -71,19 +85,21 @@ internal abstract class CacheStorePolicy : ICacheStorePolicy {
      */
     override suspend fun updateLastRequest(identity: CacheIdentity, timestamp: Instant) {
         withContext(Dispatchers.IO) {
-            localSource.upsert(
-                CacheEntity(
-                    request = request,
-                    cacheItemId = identity.id,
-                    timestamp = timestamp
-                )
+            val entity = CacheEntity(
+                request = request,
+                cacheItemId = identity.id,
+                timestamp = timestamp
             )
+            Timber.v("updateLastRequest(identity: $identity, timestamp: $timestamp)")
+            localSource.insertOrUpdate(entity)
         }
     }
 
     /**
      * Invalidates cache records for the given [identity]
      */
-    override suspend fun invalidateLastRequest(identity: CacheIdentity) =
+    override suspend fun invalidateLastRequest(identity: CacheIdentity) {
+        Timber.v("invalidateLastRequest(identity: $identity)")
         updateLastRequest(identity, Instant.EPOCH)
+    }
 }