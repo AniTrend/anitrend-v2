971d65765cd57536bb169c7f992383719389f0b3
Maxwell <mxt.developer@gmail.com>
2021-04-12 17:46:04 +0200

    Add custom list local data component

---
+++ app-data/src/main/kotlin/co/anitrend/data/customlist/datasource/CustomListLocalSource.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2021  AniTrend
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.anitrend.data.customlist.datasource
+
+import androidx.room.Dao
+import androidx.room.Query
+import co.anitrend.data.android.source.AbstractLocalSource
+import co.anitrend.data.customlist.entity.CustomListEntity
+
+@Dao
+internal abstract class CustomListLocalSource : AbstractLocalSource<CustomListEntity>() {
+
+    @Query("""
+        select count(id) from custom_list
+    """)
+    abstract override suspend fun count(): Int
+
+    @Query("""
+        delete from custom_list
+    """)
+    abstract override suspend fun clear()
+
+    @Query("""
+        delete from custom_list
+        where user_id = :userId and list_name = :listName
+    """)
+    abstract suspend fun clear(
+        userId: Long,
+        listName: String
+    )
+}
