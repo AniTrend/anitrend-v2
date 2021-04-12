e7470aa5c079bc677853f9ed5b703cd339eb91d9
Maxwell <mxt.developer@gmail.com>
2021-04-12 17:46:21 +0200

    Add custom score data component

---
+++ app-data/src/main/kotlin/co/anitrend/data/customscore/datasource/CustomScoreLocalSource.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2021  AniTrend
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.anitrend.data.customscore.datasource
+
+import androidx.room.Dao
+import androidx.room.Query
+import co.anitrend.data.android.source.AbstractLocalSource
+import co.anitrend.data.customscore.entity.CustomScoreEntity
+
+@Dao
+internal abstract class CustomScoreLocalSource : AbstractLocalSource<CustomScoreEntity>() {
+
+    @Query("""
+        select count(id) from custom_score
+    """)
+    abstract override suspend fun count(): Int
+
+    @Query("""
+        delete from custom_score
+    """)
+    abstract override suspend fun clear()
+
+    @Query("""
+        delete from custom_score
+        where user_id = :userId and score_name = :scoreName
+    """)
+    abstract suspend fun clear(
+        userId: Long,
+        scoreName: String
+    )
+}
---
+++ app-data/src/main/kotlin/co/anitrend/data/customscore/entity/CustomScoreEntity.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2021  AniTrend
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.anitrend.data.customscore.entity
+
+import androidx.room.*
+import co.anitrend.data.core.common.Identity
+import co.anitrend.data.medialist.entity.MediaListEntity
+import co.anitrend.data.user.entity.UserEntity
+import co.anitrend.support.query.builder.annotation.EntitySchema
+
+@Entity(
+    tableName = "custom_score",
+    foreignKeys = [
+        ForeignKey(
+            entity = MediaListEntity::class,
+            onDelete = ForeignKey.CASCADE,
+            onUpdate = ForeignKey.CASCADE,
+            childColumns = ["media_list_id"],
+            parentColumns = ["id"],
+            deferred = true
+        ),
+        ForeignKey(
+            entity = UserEntity::class,
+            onDelete = ForeignKey.CASCADE,
+            onUpdate = ForeignKey.CASCADE,
+            childColumns = ["user_id"],
+            parentColumns = ["id"],
+            deferred = true
+        )
+    ],
+    indices = [
+        Index(
+            value = [
+                "media_list_id",
+                "score_name",
+                "user_id"
+            ],
+            unique = true
+        )
+    ]
+)
+@EntitySchema
+internal data class CustomScoreEntity(
+    @ColumnInfo(name = "user_id") val userId: Long,
+    @ColumnInfo(name = "score") val score: Float,
+    @ColumnInfo(name = "score_name") val scoreName: String,
+    @ColumnInfo(name = "media_list_id") val mediaListId: Long,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") override val id: Long = 0
+) : Identity
