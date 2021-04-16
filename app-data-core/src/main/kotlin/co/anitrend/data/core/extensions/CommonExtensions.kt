0e27dbe09d1be23cdbb930acc7a7ec8d59ef1757
Maxwell <mxt.developer@gmail.com>
2021-04-16 11:55:03 +0200

    Add hash code mask extension for data module

---
+++ app-data-core/src/main/kotlin/co/anitrend/data/core/extensions/CommonExtensions.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2021  AniTrend
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.anitrend.data.core.extensions
+
+/**
+ * Hash code generator with the ability to mask negative values
+ */
+fun String.toHashId(): Long {
+    val hash = hashCode()
+    if (hash < 0)
+        return ((hash and 0x7fffffff)).toLong()
+    return hash.toLong()
+}
