d59f2ea2791cd0a2eb40e8dd3a2346b72f040503
Maxwell <mxt.developer@gmail.com>
2021-04-21 11:41:31 +0200

    Add spotless plugin and extensions for applying project settings

---
+++ buildSrc/src/main/java/co/anitrend/buildSrc/plugins/components/ProjectSpotless.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2021  AniTrend
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.anitrend.buildSrc.plugins.components
+
+import co.anitrend.buildSrc.extensions.spotlessExtension
+import co.anitrend.buildSrc.common.Versions
+import org.gradle.api.Project
+
+internal fun Project.configureSpotless(): Unit = spotlessExtension().run {
+    kotlin {
+        target("**/*.kt")
+        targetExclude("$buildDir/**/*.kt", "bin/**/*.kt")
+        ktlint(Versions.ktlint).userData(
+            mapOf("android" to "true")
+        )
+        licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
+    }
+}
