8f0817cc13dae56ffb42c974755e608b992c9141
Maxwell <mxt.developer@gmail.com>
2021-05-05 15:00:42 +0200

    Bump androidx.recyclerview:recyclerview [1.2.0-rc01 -> 1.2.0]
    See: https://developer.android.com/jetpack/androidx/releases/recyclerview#1.2.0

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -260,7 +260,7 @@ object Libraries {
         }
 
         object Recycler {
-            private const val version = "1.2.0-rc01"
+            private const val version = "1.2.0"
             const val recyclerView = "androidx.recyclerview:recyclerview:$version"
 
             object Selection {
