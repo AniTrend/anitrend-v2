7e36780242fedf652b110fdcbe550b07be5f08f3
Maxwell <mxt.developer@gmail.com>
2021-05-26 10:33:49 +0200

    Bump build tools version [7.0.0-alpha15 -> 7.0.0-beta02]

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -54,7 +54,7 @@ object Libraries {
     object Android {
 
         object Tools {
-            private const val version = "7.0.0-alpha15"
+            private const val version = "7.0.0-beta02"
             const val buildGradle = "com.android.tools.build:gradle:${version}"
         }
     }
