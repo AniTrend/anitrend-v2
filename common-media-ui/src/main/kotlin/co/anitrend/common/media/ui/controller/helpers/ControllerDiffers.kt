4fdc9884cbf275c729ba67bd7f538ff8652bac9b
Maxwell <mxt.developer@gmail.com>
2021-06-19 10:40:09 +0200

    Expand on media equality check to include underlying media list

--- common-media-ui/src/main/kotlin/co/anitrend/common/media/ui/controller/helpers/ControllerDiffers.kt
+++ common-media-ui/src/main/kotlin/co/anitrend/common/media/ui/controller/helpers/ControllerDiffers.kt
@@ -29,7 +29,7 @@ internal object MediaDiffUtil : DiffUtil.ItemCallback<Media>() {
         oldItem: Media,
         newItem: Media
     ): Boolean {
-        return oldItem.id == newItem.id
+        return oldItem.id == newItem.id && oldItem.mediaList?.id == newItem.mediaList?.id
     }
 
     override fun areContentsTheSame(
@@ -37,6 +37,26 @@ internal object MediaDiffUtil : DiffUtil.ItemCallback<Media>() {
         newItem: Media
     ): Boolean {
         return oldItem.hashCode() == newItem.hashCode()
+    }
+
+    /**
+     * When [.areItemsTheSame] returns `true` for two items and
+     * [.areContentsTheSame] returns false for them, this method is called to
+     * get a payload about the change.
+     *
+     * For example, if you are using DiffUtil with [RecyclerView], you can return the
+     * particular field that changed in the item and your
+     * [ItemAnimator][RecyclerView.ItemAnimator] can use that
+     * information to run the correct animation.
+     *
+     * Default implementation returns `null`.
+     *
+     * @see Callback.getChangePayload
+     */
+    override fun getChangePayload(oldItem: Media, newItem: Media): Any? {
+        return if (oldItem.mediaList == newItem.mediaList)
+            super.getChangePayload(oldItem, newItem)
+        else newItem
     }
 }
 
