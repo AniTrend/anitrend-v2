abaa58b122d7b778839cf32df989d220a4708014
Maxwell <mxt.developer@gmail.com>
2021-11-01 14:53:34 +0200

    Add new component to allow sort order toggles

---
+++ common-shared-ui/src/main/kotlin/co/anitrend/common/shared/ui/view/chip/MaterialSortChip.kt
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2021  AniTrend
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package co.anitrend.common.shared.ui.view.chip
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import co.anitrend.arch.extension.ext.getCompatDrawable
+import co.anitrend.arch.ui.view.contract.CustomView
+import co.anitrend.common.shared.R
+import co.anitrend.domain.common.enums.contract.ISortable
+import co.anitrend.domain.common.sort.order.SortOrder
+import co.anitrend.navigation.model.sorting.Sorting
+import com.airbnb.paris.extensions.style
+import com.google.android.material.chip.Chip
+
+class MaterialSortChip @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = com.google.android.material.R.attr.chipStyle
+) : Chip(context, attrs, defStyleAttr), CustomView {
+
+    private var checkedState: CheckedState
+
+    val sortOrder: SortOrder
+        get() = when (checkedState) {
+            CheckedState.ASC -> SortOrder.ASC
+            else -> SortOrder.DESC
+        }
+
+    init {
+        checkedState = CheckedState.NONE
+        onInit(context, attrs, defStyleAttr)
+    }
+
+    /**
+     * Updates internal sorting state for the current chip
+     *
+     * @see Sorting
+     * @see ISortable
+     */
+    fun <T : ISortable> updateSortOrder(sorting: Sorting<T>) {
+        checkedState = when (sorting.order) {
+            SortOrder.ASC -> CheckedState.ASC
+            SortOrder.DESC -> CheckedState.DESC
+        }
+        isChecked = true
+    }
+
+    private fun updateCheckedIconOn(checked: Boolean) {
+        if (checked) {
+            when (checkedState) {
+                CheckedState.NONE -> {
+                    checkedState = CheckedState.DESC
+                    checkedIcon = context.getCompatDrawable(
+                        R.drawable.ic_arrow_down
+                    )
+                }
+                CheckedState.ASC -> {
+                    checkedState = CheckedState.NONE
+                    checkedIcon = context.getCompatDrawable(
+                        R.drawable.ic_arrow_down
+                    )
+                }
+                CheckedState.DESC -> {
+                    checkedState = CheckedState.ASC
+                    checkedIcon = context.getCompatDrawable(
+                        R.drawable.ic_arrow_up
+                    )
+                }
+            }
+        } else {
+            checkedIcon = null
+        }
+    }
+
+    private fun onClickedAction(view: View) {
+        isChecked = when (checkedState) {
+            CheckedState.NONE -> false
+            CheckedState.DESC -> true
+            CheckedState.ASC -> true
+        }
+    }
+
+    override fun setChecked(checked: Boolean) {
+        updateCheckedIconOn(checked)
+        super.setChecked(checked)
+    }
+
+    /**
+     * Callable in view constructors to perform view inflation and attribute initialization
+     *
+     * @param context view context
+     * @param attrs view attributes if applicable
+     * @param styleAttr style attribute if applicable
+     */
+    override fun onInit(context: Context, attrs: AttributeSet?, styleAttr: Int?) {
+        style(R.style.AppTheme_Material_Chip_Action)
+        setChipBackgroundColorResource(
+            R.color.selector_chip_background
+        )
+        setOnCheckedChangeListener { buttonView, isChecked ->
+            onClickedAction(buttonView)
+        }
+        isCheckable = true
+    }
+
+    /**
+     * Should be called on a view's detach from window to unbind or release object references
+     * and cancel all running coroutine jobs if the current view
+     *
+     * Consider calling this in [android.view.View.onDetachedFromWindow]
+     */
+    override fun onViewRecycled() {
+        super.onViewRecycled()
+        setOnCheckedChangeListener(null)
+    }
+
+    /**
+     * This is called when the view is detached from a window.  At this point it
+     * no longer has a surface for drawing.
+     *
+     * @see .onAttachedToWindow
+     */
+    override fun onDetachedFromWindow() {
+        onViewRecycled()
+        super.onDetachedFromWindow()
+    }
+
+    enum class CheckedState {
+        NONE,
+        ASC,
+        DESC
+    }
+}
