37aa551780deb37ce2f3e608f156596a9549a8e3
Maxwell <mxt.developer@gmail.com>
2021-12-09 21:37:49 +0200

    Update navigation extensions
    Adds new extension function to serialise work manager parameters and helper for crating unique one time requests

--- app-navigation/src/main/kotlin/co/anitrend/navigation/extensions/NavigationExtensions.kt
+++ app-navigation/src/main/kotlin/co/anitrend/navigation/extensions/NavigationExtensions.kt
@@ -22,9 +22,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.collection.LruCache
 import androidx.core.os.bundleOf
+import androidx.work.*
 import co.anitrend.navigation.model.NavPayload
 import co.anitrend.navigation.model.common.IParam
 import co.anitrend.navigation.router.NavigationRouter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import timber.log.Timber
 
 private val classCache = object : LruCache<String, Class<*>>(8) {
@@ -125,3 +128,55 @@ fun IParam.asBundle() =
  * @return [NavPayload]
  */
 fun IParam.asNavPayload() = NavPayload(idKey, this)
+
+
+/**
+ * [Data.Builder] creator from [IParam] objects, this typically applies to tasks modules
+ *
+ * @see IParam
+ */
+fun <T : IParam> T.toDataBuilder(): Data.Builder {
+    val map = mapOf(
+        idKey to Gson().toJson(this)
+    )
+    return Data.Builder().putAll(map)
+}
+
+/**
+ * Object creator of type [IParam] that uses [WorkerParameters] to retrieve data
+ *
+ * @param contract The key from [IParam.IKey]
+ *
+ * @see IParam
+ */
+inline fun <reified T: IParam> WorkerParameters.fromWorkerParameters(contract: IParam.IKey): T {
+    val map = inputData.keyValueMap
+    return Gson().fromJson(
+        map[contract.KEY] as String,
+        object : TypeToken<T>(){}.type
+    )
+}
+
+/**
+ * Creates a [OneTimeWorkRequest] for this worker
+ *
+ * @param context Any valid application context
+ * @param params Parameters for this worker
+ *
+ * @return [WorkContinuation]
+ */
+fun Class<out ListenableWorker>.createOneTimeUniqueWorker(
+    context: Context,
+    params: IParam
+): WorkContinuation {
+    val oneTimeRequest = OneTimeWorkRequest.Builder(this)
+        .setInputData(params.toDataBuilder().build())
+        .build()
+
+    return WorkManager.getInstance(context)
+        .beginUniqueWork(
+            simpleName,
+            ExistingWorkPolicy.KEEP,
+            oneTimeRequest
+    )
+}
