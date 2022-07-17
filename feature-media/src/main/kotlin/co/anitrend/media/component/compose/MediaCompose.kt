4cc5476d50da5c1eb49cb7346c2da8acf28749a5
Maxwell <mxt.developer@gmail.com>
2022-07-17 21:53:01 +0200

    Remove place holder design elements

--- feature-media/src/main/kotlin/co/anitrend/media/component/compose/MediaCompose.kt
+++ feature-media/src/main/kotlin/co/anitrend/media/component/compose/MediaCompose.kt
@@ -19,94 +19,13 @@
 
 package co.anitrend.media.component.compose
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import co.anitrend.common.genre.ui.compose.GenresListComponent
 import co.anitrend.core.android.compose.AniTrendTheme
 import co.anitrend.media.component.viewmodel.state.MediaState
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-
-@Composable
-private fun BottomSheetContentComponent(
-    state: MediaState,
-    scope: CoroutineScope,
-    scaffoldState: BottomSheetScaffoldState
-) {
-    val mediaState by state.model.observeAsState()
-    Box(
-        Modifier
-            .fillMaxWidth()
-            .height(128.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Text("Swipe up to expand sheet")
-    }
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .padding(64.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        GenresListComponent(genres = mediaState?.genres?.toList().orEmpty())
-        Spacer(Modifier.height(20.dp))
-        Button(
-            onClick = {
-                scope.launch { scaffoldState.bottomSheetState.collapse() }
-            }
-        ) {
-            Text("Click to collapse sheet")
-        }
-    }
-}
-
-@Composable
-private fun FloatingActionButtonComponent(
-    scope: CoroutineScope,
-    scaffoldState: BottomSheetScaffoldState
-) {
-    FloatingActionButton(
-        onClick = {
-            scope.launch {
-                scaffoldState.snackbarHostState.showSnackbar("Snackbar #")
-            }
-        }
-    ) {
-        Icon(Icons.Rounded.Favorite, contentDescription = "Localized description")
-    }
-}
-
-@Composable
-private fun BottomSheetComponent(scope: CoroutineScope, state: MediaState) {
-    val scaffoldState = rememberBottomSheetScaffoldState()
-    BottomSheetScaffold(
-        sheetContent = {
-            BottomSheetContentComponent(state, scope, scaffoldState)
-        },
-        scaffoldState = scaffoldState,
-        floatingActionButton = {
-            FloatingActionButtonComponent(scope, scaffoldState)
-        },
-        floatingActionButtonPosition = FabPosition.End,
-        sheetPeekHeight = 128.dp
-    ) {
-
-    }
-}
 
 @Composable
 fun MediaDetailComponent(state: MediaState) {
     AniTrendTheme {
-        val scope = rememberCoroutineScope()
-        BottomSheetComponent(scope, state)
+        // TODO: Implement mobile overview designs
     }
 }
