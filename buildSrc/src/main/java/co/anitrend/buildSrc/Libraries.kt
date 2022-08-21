f0f6611f87c4589907ac0a0cc5ee48497ce3b213
Maxwell <mxt.developer@gmail.com>
2022-08-21 12:36:30 +0200

    Bump com.google.code.gson:gson [2.9.0 -> 2.9.1]

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -503,7 +503,7 @@ object Libraries {
         }
 
         object Gson {
-            private const val version = "2.9.0"
+            private const val version = "2.9.1"
             const val gson = "com.google.code.gson:gson:$version"
         }
 
