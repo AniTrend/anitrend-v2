ac0daba239c014bb41e4c16cb7dadeca0f5d7d9f
Maxwell <mxt.developer@gmail.com>
2022-08-21 12:27:16 +0200

    Bump androidx.fragment:fragment [1.5.0 -> 1.5.2]
    Changelog: https://developer.android.com/jetpack/androidx/releases/fragment#1.5.2

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -161,7 +161,7 @@ object Libraries {
         }
 
         object Fragment {
-            private const val version = "1.5.0"
+            private const val version = "1.5.2"
             const val fragment = "androidx.fragment:fragment:$version"
             const val fragmentKtx = "androidx.fragment:fragment-ktx:$version"
             const val test = "androidx.fragment:fragment-ktx:fragment-testing$version"
