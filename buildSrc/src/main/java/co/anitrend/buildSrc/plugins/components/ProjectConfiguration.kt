26a3f82bd65a3103fb9cba426d836c5e6f85a130
Maxwell <mxt.developer@gmail.com>
2022-09-17 15:44:43 +0200

    Resolve API deprecation warnings

--- buildSrc/src/main/java/co/anitrend/buildSrc/plugins/components/ProjectConfiguration.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/plugins/components/ProjectConfiguration.kt
@@ -25,7 +25,7 @@ import com.android.build.gradle.internal.dsl.DefaultConfig
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 import java.io.File
 
 private fun Project.configureBuildFlavours() {
