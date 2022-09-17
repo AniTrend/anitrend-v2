e868469a60053aceac2f9028ae2ba885862abfdb
Maxwell <mxt.developer@gmail.com>
2022-09-17 17:02:13 +0200

    Bump com.uwetrottmann.trakt5:trakt-java [6.10.0 -> 6.11.2]
    Changelog: https://github.com/UweTrottmann/trakt-java/blob/main/CHANGELOG.md#6112

--- buildSrc/src/main/java/co/anitrend/buildSrc/common/Versions.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/common/Versions.kt
@@ -36,7 +36,7 @@ object Versions {
     const val jsoup = "1.15.3"
 
     const val tmdb = "2.7.0"
-    const val trakt = "6.10.0"
+    const val trakt = "6.11.2"
 
     const val elements = "1.0.1"
 
