24ce649a1b7b4ddf41d00c2e701f1d1e6ce78a12
Maxwell <mxt.developer@gmail.com>
2022-12-18 17:12:04 +0200

    Migrate deprecated dependency

--- buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/Libraries.kt
@@ -44,7 +44,7 @@ object Libraries {
 
     const val retrofitSerializer = "com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:${Versions.serializationConverter}"
 
-    const val deeplink = "com.hellofresh.android:deeplink:${Versions.deeplink}"
+    const val deeplink = "com.kingsleyadio.deeplink:deeplink:${Versions.deeplink}"
 
     object Repositories {
         const val jitPack = "https://www.jitpack.io"
