1f3d9ac684917988035dde7aa84504971404e09a
Maxwell <mxt.developer@gmail.com>
2023-04-01 22:47:59 +0200

    Refactor and add new interceptor for api calls

---
+++ app-data-core/src/main/kotlin/co/anitrend/data/core/app/IAppInfo.kt
@@ -0,0 +1,10 @@
+package co.anitrend.data.core.app
+
+interface IAppInfo {
+    val locale: String
+    val version: String
+    val source: String
+    val code: String
+    val label: String
+    val buildType: String
+}
