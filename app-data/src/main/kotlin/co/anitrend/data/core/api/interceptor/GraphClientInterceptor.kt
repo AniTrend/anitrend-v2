dbf86ced39877b7f96b9d13c9f830c62aefb5bbe
Maxwell Mapako <wax911@users.noreply.github.com>
2024-06-03 15:01:26 +0200

    chore: update GraphClientInterceptor.kt

--- app-data/src/main/kotlin/co/anitrend/data/core/api/interceptor/GraphClientInterceptor.kt
+++ app-data/src/main/kotlin/co/anitrend/data/core/api/interceptor/GraphClientInterceptor.kt
@@ -37,7 +37,7 @@ internal class GraphClientInterceptor(
         val contentLength = original.body?.contentLength() ?: 0
         val requestBuilder = original.newBuilder()
 
-        requestBuilder.header(CONTENT_TYPE, GraphConverter.MimeType)
+        requestBuilder.header(CONTENT_TYPE, GraphConverter.MIME_TYPE)
             .header(ACCEPT, ACCEPT_TYPE)
             .header(CONTENT_LENGTH, contentLength.toString())
             .method(original.method, original.body)
