9c3d85e5ab5115cac9f97629fea21ee53aea05b8
Maxwell <mxt.developer@gmail.com>
2025-02-17 22:48:53 +0200

    refactor: move initial shortcut creation to application initializer
    Remove the dependency on `android-onboarding` to generate initials shortcuts

--- app/src/main/kotlin/co/anitrend/initializer/ApplicationInitializer.kt
+++ app/src/main/kotlin/co/anitrend/initializer/ApplicationInitializer.kt
@@ -17,13 +17,32 @@
 package co.anitrend.initializer
 
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.startup.Initializer
+import co.anitrend.arch.extension.preference.contract.ISupportPreference
+import co.anitrend.core.android.koinOf
+import co.anitrend.core.android.shortcut.contract.IShortcutController
+import co.anitrend.core.android.shortcut.model.Shortcut
 import co.anitrend.core.initializer.contract.AbstractCoreInitializer
 import co.anitrend.core.initializer.injector.InjectorInitializer
 import co.anitrend.core.koin.helper.DynamicFeatureModuleHelper.Companion.loadModules
 import co.anitrend.koin.appModules
+import timber.log.Timber
 
 class ApplicationInitializer : AbstractCoreInitializer<Unit>() {
+
+    @RequiresApi(Build.VERSION_CODES.N_MR1)
+    private fun setUpShortcuts() {
+        runCatching {
+            val controller = koinOf<IShortcutController>()
+            controller.createShortcuts(
+                Shortcut.AiringSchedule(),
+                Shortcut.Search(),
+            )
+        }.onFailure(Timber::w)
+    }
+
     /**
      * Initializes and a component given the application [Context]
      *
@@ -31,6 +50,12 @@ class ApplicationInitializer : AbstractCoreInitializer<Unit>() {
      */
     override fun create(context: Context) {
         appModules.loadModules()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            val settings = koinOf<ISupportPreference>()
+            if (settings.isNewInstallation.value) {
+                setUpShortcuts()
+            }
+        }
     }
 
     /**
