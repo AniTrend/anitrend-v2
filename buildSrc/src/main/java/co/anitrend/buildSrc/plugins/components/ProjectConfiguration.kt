5b69ef5aa223ead94af9731d999decdb9572ee58
Maxwell <mxt.developer@gmail.com>
2025-02-19 19:58:23 +0200

    chore: bump JVM target from 17 to 21

--- buildSrc/src/main/java/co/anitrend/buildSrc/plugins/components/ProjectConfiguration.kt
+++ buildSrc/src/main/java/co/anitrend/buildSrc/plugins/components/ProjectConfiguration.kt
@@ -168,13 +168,13 @@ internal fun Project.configureAndroid(): Unit = baseExtension().run {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     tasks.withType(KotlinJvmCompile::class.java) {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
 
